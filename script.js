4d1ae3e2e403ade5f5f7a1e1b7ebc7bf94265419
iagoplz123 <106621047+iagoplz123@users.noreply.github.com>
2023-06-24 23:24:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Scroll suave para âncoras
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
